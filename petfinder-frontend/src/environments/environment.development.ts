46772f91cfc38eadcce3b3131467ed58f241d6f0
delzilwilfred <henrydaniel2527@gmail.com>
2025-10-25 15:49:54 +0530

    few changes1

--- petfinder-frontend/src/environments/environment.development.ts
+++ petfinder-frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,  // ✅ Change to false for development
+    production: true,  // ✅ Change to false for development
     apiUrl: 'http://localhost:5000/v1/users'  // ✅ Change port to 5000
 };
